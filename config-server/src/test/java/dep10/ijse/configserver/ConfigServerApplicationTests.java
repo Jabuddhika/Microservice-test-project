1007dd475f6c7b304b1b8540ea58d4065a9a35c1
jabsampath <jab.sampath@gmil.com>
2023-12-12 19:04:08 +0530

    discovery server is created and tested

---
+++ config-server/src/test/java/dep10/ijse/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dep10.ijse.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ discovery/src/test/java/dep10/ijse/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package dep10.ijse.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
